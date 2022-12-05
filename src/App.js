9e453f921cd20d247c8047a35e9cfe7c9ede9206
MalteKrog <90311355+MalteKrog@users.noreply.github.com>
2022-12-05 10:15:16 +0100

    Nav

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import { Route, Routes, NavLink } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
+import ContactPage from './pages/ContactPage';
+import ProductPage from './pages/ProductPage';
 
 function App() {
   return (
-    <div className="App">
-      
+    <div>
+      <NavLink to="/">
+          Hjem
+      </NavLink>
+      <NavLink to="/products">
+          Produkter
+      </NavLink>
+      <NavLink to="/about">
+          Om os
+      </NavLink>
+      <NavLink to="/contact">
+          Kontakt
+      </NavLink>            
+      <Routes>
+        <Route exact path="/" element={<HomePage />} />
+        <Route exact path="/products" element={<ProductPage />} />
+        <Route exact path="/about" element={<AboutPage />} />
+        <Route exact path="/contact" element={<ContactPage />} />
+      </Routes>
     </div>
   );
 }
