4b1bc6fd2149bcee97468c5c0d6efdc5da3634d8
MathiasQM <mathiasqm@gmail.com>
2022-12-09 10:05:02 +0100

    homepage

---
+++ src/components/Homepage/værdier/VærdierMission.js
@@ -0,0 +1,33 @@
+import React from 'react'
+
+import './værdier.css'
+
+const VærdierMission = () => {
+  return (
+    <section className='vaerdier-mission'>
+        <div className='mission-cards'>
+            <div className='mission-banner'>
+                <img src='' alt='Et eller andet billede'/>
+                <div className='banner-tekst'>
+                    <h2>Vores værdier og misson</h2>
+                    <p>kljeralsjkfhlæidfjdslæidqhloaisæj laksdfchj alæskdhjfc ælaskjhclkdva ihjælj JLKSH IH KLÆD</p>
+                </div>
+            </div>
+                <div className='mission-card test'>
+                    <h3>Gratis og hurtig levering!</h3>
+                    <p>Vi leverer dine varer helt gratis når du handler for over 3000 kr.</p>
+                </div>
+                <div className='mission-card'>
+                    <h3>De bedste varer</h3>
+                    <p>Vi har de bedste varer til de bedste priser</p>
+                </div>
+                <div className='mission-card test2'>
+                    <h3>God kundeservice</h3>
+                    <p>Vi går op i vores kunder og stræber efter at give dig den bedste service </p>
+                </div>
+        </div>
+    </section>
+  )
+}
+
+export default VærdierMission
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,10 +1,16 @@
 import React from 'react'
+import Kurv from '../components/Kurv/Kurv.js'
+import VideoBanner from '../components/Homepage/videobanner/VideoBanner'
+import VærdierMission from '../components/Homepage/værdier/VærdierMission.js'
 
+import '../components/Homepage/homepage.css'
 const HomePage = () => {
   return (
-    <>
-    <div>HomePage</div>
-    </>
+    <main>
+      <VideoBanner />
+      <VærdierMission />
+      <Kurv />
+    </main>
   )
 }
 
