97ef783e03118abeab76c171f6acac9b94b63542
MathiasQM <mathiasqm@gmail.com>
2022-12-12 18:12:21 +0100

    Product page

--- src/components/Products/ProductCard.js
+++ src/components/Products/ProductCard.js
@@ -1,32 +1,62 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {BsArrowRight} from 'react-icons/bs'
+import {BsSnow} from 'react-icons/bs'
+import {TbTruck,TbTruckOff} from 'react-icons/tb'
 
 import './products.css'
+import SingleProductInfo from './SingleProductInfo'
 
-const ProductCard = () => {
+const ProductCard = ({ product }) => {
+  const stockAmmount = product.acf.stockammount
+  const category = product.acf.category
+
+  const [showDialog, setShowDialog] = useState(false);
+  
   return (
-    <div className='product-card-container'>
-      <div className='product-image-container'>
+    <>
+      <div onClick={() => setShowDialog(true)} className='product-card-container'>
 
-        <img />
-        <div className='product-infobar-container'>
+        <div className='product-image-container'>
+
+          <img src={product.acf.productimage.url} alt={product.acf.productimage.alt}/>
+          <div className='product-infobar-container'>
+            {category === "frost" ?
+            <div className='icon-frost'>
+              <BsSnow />
+              <p>Frost</p>
+            </div>: null
+            }
+            
+            {stockAmmount < 0 ?
+                <div className='icon-lager'>
+                <TbTruck />
+                <p>På lager</p>
+                </div> :
+                  <div className='icon-lager'>
+                  <TbTruckOff />
+                  <p>Ikke på lager</p>
+                  </div> 
+            }
+
+          </div>
 
         </div>
 
-      </div>
+        <div className='product-info-container'>
+          <div>
+            <h3 className='product-card-title'>{product.acf.producttitle}</h3>
+            <h3 className='price'>{product.acf.price}kr.</h3>
+          </div>
+          
+          <p>{product.acf.description}</p>
+          <button className='btn-2'>See more <BsArrowRight/></button>
 
-      <div className='product-info-container'>
-        <div>
-          <h3 className='product-title'>Product title</h3>
-          <h3 className='price'>Price</h3>
         </div>
-        
-        <p>Description</p>
-        <button className='btn-2'>See more <BsArrowRight/></button>
+
 
       </div>
-
-    </div>
+            <SingleProductInfo showDialog={showDialog} setShowDialog={setShowDialog} key={product.id} product={product} />
+    </>
   )
 }
 
