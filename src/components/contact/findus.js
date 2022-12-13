10b66ac8164a6cee88fd82da35d0ab3948088ef9
Louise Christiansen <eaalgch@students.eaaa.dk>
2022-12-13 13:43:52 +0100

    Kontakt færdig
    :)

--- src/components/contact/findus.js
+++ src/components/contact/findus.js
@@ -5,7 +5,7 @@ const Findus = () => {
         <div className='map-container'>
             <div className='tekst-container'>
                 <h2 className='maph2'>Adresse</h2>
-                <h2 className='maph2'>Du kan finde os på følgende addresse</h2> 
+                <h2 className='maph1'>Du kan finde os på følgende addresse</h2> 
             </div>
             <div className='map'>
                 <div class="mapouter"><div class="gmap_canvas"><iframe width="100%" height="500px" id="gmap_canvas" src="https://maps.google.com/maps?q=Krogsg%C3%A5rdsvej%206%208620%20Kjellerup&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe></div></div>
