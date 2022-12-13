9197dac8454d302acc610e7cab0fcdd05c66db85
Cthrige <cthrige@gmail.com>
2022-12-13 08:29:23 +0100

    Contact components
    Ti louise
    Findus - Er map og tekst
    Contact er kontaktformular og bokse
    De mangler lidt styling og noget responsivenes,
    Jeg har ikke stylet dem i andet end app.css. Måske det også bør ændres, i dont know

--- src/components/contact/contact.js
+++ src/components/contact/contact.js
@@ -4,8 +4,26 @@ import 'animate.css';
 import TrackVisibility from 'react-on-screen';
 import React, { useRef } from 'react';
 import emailjs from '@emailjs/browser';
+import PhoneInput from 'react-phone-number-input'
 
-/* Startet på en kontakt formular, den virker til min mail cthrige@gmail.com */
+
+/* Prøver at bruge et library kaldet "React phone number input - 
+søg evt på nettet efter det - virker ikke så godt pt" */
+
+const Reactphone = () => {
+  const [value, setValue] = useState();
+return (
+  <PhoneInput
+    placeholder="Enter phone number"
+    value={value}
+    onChange={setValue}/>
+)
+};
+
+export default Reactphone;
+
+/* Startet på en kontakt formular, den virker til min mail cthrige@gmail.com
+Kan vi godt fortsætte med, bare ik spam mig hehe */
 
 export const Contact = () => {
   const form = useRef();
@@ -24,27 +42,49 @@ export const Contact = () => {
 
   return (
     <section className="contact" id="connect">
+      <h1 className="kontakttitle">Kontakt</h1>
       <Container>
-        <Row className="align-items-center">
-          <Col size={12} md={6}>
-            <TrackVisibility>
-              {({ isVisible }) =>
-                <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
-                <h2>Kontakt mig!</h2>
-                  <Row>
-                    <form ref={form} onSubmit={sendEmail}>
-                    <label>Name</label>
-                    <input type="text" name="from_name" />
-                    <label>Email</label>
-                    <input type="email" name="email" />
-                    <label>Message</label>
-                    <textarea name="message" />
-                    <input className="kontaktbtn" type="submit" value="Send" />
-                  </form>
-                  </Row>
-              </div>}
-            </TrackVisibility>
-          </Col>
+        <Row className="contactcontainer">
+            <Col>
+              <TrackVisibility>
+                {({ isVisible }) =>
+                  <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
+                  <h2>Kontakt mig!</h2>
+                    <Row>
+                      <form ref={form} onSubmit={sendEmail}>
+                      <label>Name</label>
+                      <input type="text" name="from_name" />
+                      <label>Email</label>
+                      <input type="email" name="email" />
+                      <label>Emne</label>
+                      <input type="text" name="from_name" />
+                      <label>Message</label>
+                      <textarea name="message" />
+                      <input className="kontaktbtn" type="submit" value="Send" />
+                    </form>
+                    </Row>
+                </div>}
+              </TrackVisibility>
+           </Col>
+           <Col>
+           <div className="kontaktkort-container">
+              <div className="kontaktcards">
+                <div className="kontaktkort kkort1">
+                  <h2>Test</h2>
+                  <p>Lidt tekst lol lol</p>
+                </div>
+                <div className="kontaktkort kkort2">
+                  <h2>Test</h2>
+                  <p>Lidt tekst lol lol</p>
+                </div>
+                <div className="kontaktkort kkort3">
+                  <h2>Test</h2>
+                  <Reactphone />
+                </div>
+              </div>
+            </div>
+            
+            </Col>
         </Row>
       </Container>
     </section>
