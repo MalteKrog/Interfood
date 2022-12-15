e8ce967cc537f410487d24df7f9d7882f4a5ee0d
Cthrige <cthrige@gmail.com>
2022-12-15 09:59:51 +0100

    about

--- src/components/About/about.js
+++ src/components/About/about.js
@@ -1,86 +1,106 @@
-import React ,{Fragment} from 'react'
-import "../About/about.css"
+import React,{Fragment,useState} from 'react'
 
 const About = () => {
-    return (
-        <Fragment>
+  const [ toggleTab, setToggleTab] = useState(1)
+  const toggleState = (index) =>{
+    setToggleTab(index)
+  }
+  return (
+    <Fragment>
+
+    <section className="about">
+
+    <div className="row">
+
+    <div className="column">
+      <div className="about-img"></div>
+    </div>
+
+    <div className="column">
+
+    <div className="tabs">
+
+    <div className={toggleTab === 1 ? "single-tab active-tab": "single-tab"}
+    onClick = {() => toggleState(1)}
+    >
+      <h2>About</h2>
+    </div>
+
+    <div className={toggleTab === 2 ? "single-tab active-tab": "single-tab"}
+    onClick = {() => toggleState(2)}
+    >
+      <h2>Værdier</h2>
+    </div>
+
+    <div className={toggleTab === 3 ? "single-tab active-tab": "single-tab"}
+    onClick = {() => toggleState(3)}
+    >
+      <h2>Experience</h2>
+    </div>
+      
+    </div>
+
+    <div className="tab-content">
+
+    {/* About Content */}
+
+    <div className={toggleTab === 1 ?"content active-content":"content"}>
+      <h2>Hvem er vi?</h2>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+       Eveniet nostrum laborum porro pariatur minima tenetur.
+       Adipisci obcaecati placeat ex veritatis est delectus sunt, 
+       quam doloribus nemo sed perferendis ipsa corporis?</p>
+       <h3>Vi sørger for høj kvalitet med endnu højere service!</h3>
+       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores, quae accusantium totam mollitia facilis debitis porro sunt repellat eveniet dignissimos.</p>
+    </div>
+
+    {/* About Content */}
+
+    <div className={toggleTab === 2 ?"content active-content":"content"}>
+      <h2>Værdier</h2>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+       Eveniet nostrum laborum porro pariatur minima tenetur.
+       Adipisci obcaecati placeat ex veritatis est delectus sunt, 
+       quam doloribus nemo sed perferendis ipsa corporis?</p>
+
+       
+
+    </div>
+
+       {/* Timeline Content? */}
+
+    <div className={toggleTab === 3 ?"content active-content":"content"}>
+
+    <div className="exp-column">
+      <h3>Interfoods begyndelse</h3>
+      <span>2017-2018</span>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim error rem dicta perferendis et qui obcaecati labore accusantium cupiditate libero.</p>
+    </div>
+
+    <div className="exp-column">
+      <h3>Tid til nye lokaler</h3>
+      <span>2019-2020</span>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim error rem dicta perferendis et qui obcaecati labore accusantium cupiditate libero.</p>
+    </div>
+
+    <div className="exp-column">
+      <h3>Interfood i 5. gear</h3>
+      <span>2022</span>
+      <p>Rigtig mange nye kunde, bla bla blaLorem ipsum dolor sit amet consectetur adipisicing elit. Enim error rem dicta perferendis et qui obcaecati labore accusantium cupiditate libero.</p>
+    </div>
+    
+    </div>
         
-        <section className='about'>
-            <div className='row'>
+    </div>
 
-                <div className='column'>
-                    <div className='about-img'>
-                </div>
+    </div>
 
-                </div>
+    </div>
 
-                <div className='column'>
-
-                <div className='tabs'>
-                    <div className='singe-tab'>
-                        <h2>About</h2>
-                    </div>
-
-                    <div className='singe-tab'>
-                        <h2>About</h2>
-                    </div>
-
-                    <div className='singe-tab'>
-                        <h2>About</h2>
-                    </div>
-                
-                </div>
-
-                <div className='tab-content'>
-
-                    {/* Historie indhold*/}
-                    <div className='content'>
-                        <h2>Historie</h2>
-                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text</p>
-                        <h3>Underoverskrift 3</h3>
-                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text</p>
-                    </div>
-
-                    {/*Noget andet indhold*/}
-                    <div className='content'>
-                        <h2>Noget andet</h2>
-                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text</p>
-                        <h3>Underoverskrift 3</h3>
-                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text</p>
-                    </div>
-
-                    {/*Noget andet indhold*/}
-                    <div className='content'>
-                        <div className='exp-column'>
-                            <h3>Sælger</h3>
-                            <span>2018-2022</span>
-                            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy te</p>     
-                        </div>
-
-                        <div className='exp-column'>
-                            <h3>Sælger</h3>
-                            <span>2018-2022</span>
-                            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy te</p>     
-                        </div>
-
-                        <div className='exp-column'>
-                            <h3>Sælger</h3>
-                            <span>2018-2022</span>
-                            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy te</p>     
-                        </div>
-
-                    </div>
- 
-                </div>
-
-                </div> 
-
-                </div>
-
-        </section>
-        
-        </Fragment>
-    )
+    </section>
+    
+    </Fragment>
+  )
 }
 
 export default About
